6498b3f9df5afebb3599d31d70f6535ff6c4fbf6
Kazkoviy <illyaboiko1807@gmail.com>
2021-08-31 22:34:57 +0300

    Updates

---
+++ precache-manifest.6269f4d6688f5431256ef65eccd93ec7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48722a03a81854ef001f7fb0fdf4cc1c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "61e7aeb5a2e3db6ff37f",
+    "url": "./static/js/2.d1ce54ea.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.d1ce54ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d35d0927a1f7cd4ffb67",
+    "url": "./static/js/main.e2c60e5e.chunk.js"
+  },
+  {
+    "revision": "994f72ec58672ee8392d",
+    "url": "./static/js/runtime-main.a5fa282b.js"
+  }
+]);
